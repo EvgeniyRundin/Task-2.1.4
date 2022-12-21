e474b8469016ebcf2e0be778f6dcac3d3019574d
Евгений <jekarundin@yandex.ru>
2022-12-21 15:44:13 +0600

    Kashey pomer;(

--- src/main/java/koschei/models/Ocean1.java
+++ src/main/java/koschei/models/Ocean1.java
@@ -5,12 +5,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Ocean1 {
-
     @Autowired
     private Island2 island;
 
     @Override
     public String toString() {
+
         return "на океане остров " + island.toString();
     }
 }
